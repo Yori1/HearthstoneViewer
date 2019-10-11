d792b930fe3d80acb041fca1712688afd9c57852
Yori Garcia Rosario <yorigr@outlook.com>
2019-10-11 14:16:54 +0200

    added environment variables prod

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,8 @@
 export const environment = {
-  production: true
+  production: false,
+  hearthstoneApi: "https://omgvamp-hearthstone-v1.p.rapidapi.com/",
+  searchEndpoint: "cards/search",
+  infoEndpoint: "info",
+  apiKey: "64557067d8msh04633377c1aea24p147f27jsnefba332962f0"
+
 };
