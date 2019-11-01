659f2c4072ad165915c0931da4b3953e734410b8
Yori Garcia Rosario <yorigr@outlook.com>
2019-11-01 13:11:32 +0100

    added tweet button

--- src/app/components/list-detail/list-detail.component.ts
+++ src/app/components/list-detail/list-detail.component.ts
@@ -13,6 +13,7 @@ import { CardDisplayingService } from '../overview/card.displaying.service';
 export class ListDetailComponent implements OnInit {
 
   public listId: string;
+  public twitterLink: string;
 
   constructor(
     private route: ActivatedRoute,
@@ -28,6 +29,21 @@ export class ListDetailComponent implements OnInit {
       this.listId = params.get("id");
       this.listService.UpdateCardsInList(this.listId);
     });
+
+    const func = (d,s,id) => {
+      var js: any,
+          fjs=d.getElementsByTagName(s)[0],
+          p='https';
+      if(!d.getElementById(id)){
+          js=d.createElement(s);
+          js.id=id;
+          js.src=p+"://platform.twitter.com/widgets.js";
+          fjs.parentNode.insertBefore(js,fjs);
+      }
+
+      this.twitterLink = 'https://twitter.com/intent/tweet?text=I%20made%20a%20list%20check%20it%20out%20';
+  }
+  func(document,"script","twitter-wjs");
   }
 }
 
